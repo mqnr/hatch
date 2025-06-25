7eec9dbff2cbe139d8a53b44d868f573bb4aaba9
mzamorano <martin@mzamorano.com>
2025-06-25 15:49:36 -0700

    reformat flake.nix somewhat

--- flake.nix
+++ flake.nix
@@ -5,46 +5,42 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "aarch64-darwin"
-    ];
-
-  forEachSystem = f:
-    nixpkgs.lib.genAttrs supportedSystems (system:
-      f { pkgs = import nixpkgs { inherit system; }; }
-    );
-  in
-  {
-    devShells = forEachSystem ({ pkgs }: {
-      default = pkgs.mkShell {
-        buildInputs = [
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.rust-analyzer
-          pkgs.zlib
-          pkgs.openssl
-        ];
-      };
-    });
-
-    packages = forEachSystem ({ pkgs }: {
-      default = pkgs.rustPlatform.buildRustPackage {
-        pname = "hatch";
-        version = "0.1.0";
-        src = ./.;
-
-        cargoLock.lockFile = ./Cargo.lock;
-
-        meta = with pkgs.lib; {
-          description = "Hatch project management software";
-          license = licenses.asl20;
-          homepage = "https://github.com/mqnr/hatch";
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs {
+          inherit system;
         };
-      };
-    });
-  };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+            pkgs.zlib
+            pkgs.openssl
+          ];
+        };
+      });
+
+      packages = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.rustPlatform.buildRustPackage {
+          pname = "hatch";
+          version = "0.1.0";
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+
+          meta = with pkgs.lib; {
+            description = "Hatch project management software";
+            license = licenses.asl20;
+            homepage = "https://github.com/mqnr/hatch";
+          };
+        };
+      });
+    };
 }
